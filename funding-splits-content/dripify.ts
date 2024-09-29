d21d948a6ccf827f3041e9dc580daf9209e405e5
nya-elimu <info@elimu.ai>
2024-09-30 01:17:10 +0700

    feat(drips): dripify (#58)
    #58

--- funding-splits-content/dripify.ts
+++ funding-splits-content/dripify.ts
@@ -18,19 +18,29 @@ for (const language of languages) {
     const columns = [ 'id', 'ethereum_address', 'impact_percentage' ]
     const stringifier = stringify({ header: true, columns: [ 'ethereum_address', 'impact_percentage' ] })
     const writeStream = fs.createWriteStream(outputPath)
+    let impactPercentageZeroAddress = 0
     fs.createReadStream(filePath)
         .pipe(parse({ from_line: 2 }))
         .on('data', (row) => {
             console.log('row:', row)
-            if (row[2] > 0) {
-                // ethereum_address,impact_percentage
-                const row_dripified = [ row[columns.indexOf('ethereum_address')], Number(row[columns.indexOf('impact_percentage')]) ]
+            const ethereumAddress = row[columns.indexOf('ethereum_address')]
+            const impactPercentage = Number(row[columns.indexOf('impact_percentage')])
+            if (impactPercentage > 0) {
+                if (ethereumAddress == '0x0000000000000000000000000000000000000000') {
+                    impactPercentageZeroAddress += impactPercentage
+                    return
+                }
+                const row_dripified = [ ethereumAddress, impactPercentage ]
                 console.log('row_dripified:', row_dripified)
                 stringifier.write(row_dripified)
             }
         })
         .on('end', () => {
             console.log('end:', filePath)
+            if (impactPercentageZeroAddress > 0) {
+                const row_dripified = [ '0x0000000000000000000000000000000000000000', impactPercentageZeroAddress ]
+                stringifier.write(row_dripified)
+            }
             stringifier.pipe(writeStream)
             console.log('Finished writing data:', filePath)
         })
